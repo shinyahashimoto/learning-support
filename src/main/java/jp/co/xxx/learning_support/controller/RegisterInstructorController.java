765b31f5caa4b2c0382ccc6e42741dba6da1b6f2
shinyahashimoto <shinya.hashimoto@rakus-partners.co.jp>
2020-01-03 17:58:00 +0900

    講師のパスワード変更機能の追加

--- src/main/java/jp/co/xxx/learning_support/controller/RegisterInstructorController.java
+++ src/main/java/jp/co/xxx/learning_support/controller/RegisterInstructorController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.xxx.learning_support.domain.Instructor;
 import jp.co.xxx.learning_support.form.InstructorForm;
-import jp.co.xxx.learning_support.repository.InstructorRepository;
 import jp.co.xxx.learning_support.service.InstructorService;
 
 @Controller
@@ -19,9 +18,9 @@ public class RegisterInstructorController {
 
 	@Autowired
 	private InstructorService service;
-	
+
 	@ModelAttribute
-	public InstructorForm setUpInstructorForm(){
+	public InstructorForm setUpInstructorForm() {
 		return new InstructorForm();
 	}
 
@@ -56,5 +55,69 @@ public class RegisterInstructorController {
 
 		return "admin/admin_training_list";
 
+	}
+
+	/**
+	 * 登録されているか確認するページに遷移します.
+	 * 
+	 * @return 確認ページ
+	 */
+	@RequestMapping("/toConfirmInstructorAccountPage")
+	public String toConfirmAccountPage() {
+		return "admin/instructor_confirm_account";
+	}
+
+	/**
+	 * 登録されているEメールとパスワードで講師情報を取得します.
+	 * 
+	 * @param form   Eメールとパスワード
+	 * @param result
+	 * @return 入力されたEメールとパスワードに該当する講師情報
+	 */
+	@RequestMapping("/confirmAccountOfInstructor")
+	public String confirmAccountOfInstructor(@Validated InstructorForm form, BindingResult result) {
+		// 必要であればエラーチェックを追加してください
+
+		if (result.hasErrors()) {
+			return "";
+		}
+
+		Instructor instructor = new Instructor();
+		BeanUtils.copyProperties(form, instructor);
+		Instructor instructorAccount = service.findInstructor(instructor);
+
+		if (instructorAccount == null) {
+			// エラー表示をして、遷移元に遷移してください
+			return "admin/instructor_confirm_account";
+		}
+
+		form.setEmail(instructorAccount.getEmail());
+		return "admin/instructor_change_password";
+	}
+
+	/**
+	 * パスワードを変更します.
+	 * 
+	 * @param form   入力されたパスワード・確認用パスワード
+	 * @param result
+	 * @return ログイン画面
+	 */
+	@RequestMapping("/changePasswordOfInstructor")
+	public String changePasswordOfInstructor(@Validated InstructorForm form, BindingResult result) {
+		// 必要であればエラーチェックを追加してください
+
+		if (!(form.getPassword().equals(form.getConfirmPassword()))) {
+			result.rejectValue("confirmPassword", "", "*入力されたパスワードと異なります");
+		}
+
+		if (result.hasErrors()) {
+			return "redirect:/instructor/toRegisterInstructorPage";
+		}
+
+		Instructor instructor = new Instructor();
+		BeanUtils.copyProperties(form, instructor);
+		service.changePassWord(instructor);
+
+		return "admin/instructor_login";
 	}
 }
--- src/main/java/jp/co/xxx/learning_support/form/InstructorForm.java
+++ src/main/java/jp/co/xxx/learning_support/form/InstructorForm.java
@@ -4,45 +4,50 @@ public class InstructorForm {
 
 	private String name;
 	private String email;
+	private String password;
+	private String confirmPassword;
 	private String affiliation;
 	private String remarks;
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
 	public String getAffiliation() {
 		return affiliation;
 	}
-
 	public void setAffiliation(String affiliation) {
 		this.affiliation = affiliation;
 	}
-
 	public String getRemarks() {
 		return remarks;
 	}
-
 	public void setRemarks(String remarks) {
 		this.remarks = remarks;
 	}
-
 	@Override
 	public String toString() {
-		return "InstructorForm [name=" + name + ", email=" + email + ", affiliation=" + affiliation + ", remarks="
-				+ remarks + "]";
+		return "InstructorForm [name=" + name + ", email=" + email + ", password=" + password + ", confirmPassword="
+				+ confirmPassword + ", affiliation=" + affiliation + ", remarks=" + remarks + "]";
 	}
 
 }
